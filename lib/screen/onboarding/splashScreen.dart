ecd38a62aa4774f572b587c464554b22b700574a
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-08-24 15:38:18 +0600

    splash screen logo

--- lib/screen/onboarding/splashScreen.dart
+++ lib/screen/onboarding/splashScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:taskmannager/style/style.dart';
 
 class splashScreen extends StatefulWidget {
   const splashScreen({super.key});
@@ -12,8 +14,16 @@ class _splashScreenState extends State<splashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Splash Screen'),
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Center(
+              child: SvgPicture.asset("assets/images/logo.svg",alignment: Alignment.center),
+            ),
+          )
+        ],
       ),
     );
   }
