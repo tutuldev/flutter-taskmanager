0430108aea67051d92df52819030a953cb9496c4
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-08-24 15:58:01 +0600

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Task Mannger",
-      initialRoute: '/',
+      // initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         '/':(context)=> splashScreen(),
         '/login':(context)=>loginScreen(),
--- lib/screen/onboarding/loginScreen.dart
+++ lib/screen/onboarding/loginScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:taskmannager/style/style.dart';
 
 class loginScreen extends StatefulWidget {
   const loginScreen({super.key});
@@ -11,6 +13,35 @@ class loginScreen extends StatefulWidget {
 class _loginScreenState extends State<loginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Get Started with", style: Head1Text(colorDarkBlue)),
+                SizedBox(height: 1),
+                Text("Learn with rabbil hasan", style: Head6Text(colorLightGray)),
+                SizedBox(height: 20),
+                TextFormField(decoration: AppInputDecoration("Email Address"),),
+                SizedBox(height: 20),
+                TextFormField(decoration: AppInputDecoration("Password"),),
+                SizedBox (height: 20),
+                Container(child: ElevatedButton(
+                  style: AppButtonStyle(),
+                  child: SuccessButtonChild('Login'),
+                  onPressed: (){
+                  },
+                ),)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
